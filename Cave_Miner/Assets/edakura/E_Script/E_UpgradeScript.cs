53d2627be29e71a8343aa4640f6d0c6d6c4f0c20
EDAMAME118 <h1202519011@hiratagakuen.onmicrosoft.com>
2026-05-12 13:20:55 +0900

    制限時間とシーン移動完成

--- Cave_Miner/Assets/edakura/E_Script/E_UpgradeScript.cs
+++ Cave_Miner/Assets/edakura/E_Script/E_UpgradeScript.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class E_UpgradeScript : MonoBehaviour
 {
-    //各種テキスト
+
+    //各種テキスト表示用変数
     public Text DiggingText;
     public Text SpeedText;
     public Text RangeText;
@@ -19,44 +20,15 @@ public class E_UpgradeScript : MonoBehaviour
 
     public string nextSceneName;
 
-    //アップグレードに必要なスコア変数(強化毎に上昇)
-    private int DiggingScore;
-    private int SpeedScore;
-    private int RangeScore;
-    //各種強化レベル
-    private int DiggingLevel;
-    private int SpeedLevel;
-    private int RangeLevel;
-
     //アップグレード通知用の変数
     private float notifyTimer = 0.0f;
     private const float DisplayDuration = 2.0f;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Start関数を一度しか動かしたくない
-        //必然的にgameObjectをTrue、Falseにする方法で
-        //シーン移動時アップグレードが作動しないようにする
-        //そのためシーン移動時Destroyしないようにする
-        DontDestroyOnLoad(this.gameObject);
-
-        //必要スコア初期化
-        DiggingScore = 100;
-        SpeedScore   = 100;
-        RangeScore   = 100;
-
-        //強化レベル初期化
-        DiggingLevel = 1;
-        SpeedLevel   = 1;
-        RangeLevel   = 1;
-
         //強化時表示されるテキストを空にしておく
-        NotifyText.text = $"";
-    }
-
-    private void OnEnable()
-    {
         NotifyText.text = $"";
     }
 
@@ -67,13 +39,13 @@ public class E_UpgradeScript : MonoBehaviour
         ScoreText.text = $"現在のスコア:{ScoreManager.score}";
 
         //必要スコア表示、
-        DiggingText.text = $"{DiggingScore}スコア";
-        SpeedText.text   = $"{SpeedScore}スコア";
-        RangeText.text   = $"{RangeScore}スコア";
+        DiggingText.text = $"{PlayerDataManager.DiggingScore}スコア";
+        SpeedText.text   = $"{PlayerDataManager.SpeedScore}スコア";
+        RangeText.text   = $"{PlayerDataManager.RangeScore}スコア";
         //強化レベル表示
-        DiggingLevelText.text = $"採掘速度 Lv{DiggingLevel}";
-        SpeedLevelText.text = $"移動速度 Lv{SpeedLevel}";
-        RangeLevelText.text = $"採掘範囲 Lv{RangeLevel}";
+        DiggingLevelText.text = $"採掘速度 Lv{PlayerDataManager.DiggingLevel}";
+        SpeedLevelText.text = $"移動速度 Lv{PlayerDataManager.SpeedLevel}";
+        RangeLevelText.text = $"採掘範囲 Lv{PlayerDataManager.RangeLevel}";
 
         //通知テキストのカウントダウン処理
         if (notifyTimer > 0)
@@ -85,15 +57,13 @@ public class E_UpgradeScript : MonoBehaviour
             }
         }
 
-        
-
         //--強化ボタン押下時の動作
         //1キー
         if (Keyboard.current.digit1Key.wasPressedThisFrame)
         {
             //--採掘速度強化
             //スコアが足りない場合
-            if(ScoreManager.score < DiggingScore )
+            if(ScoreManager.score < PlayerDataManager.DiggingScore )
             {
                 ShowNotify($"スコアが足りません");
             }
@@ -101,13 +71,13 @@ public class E_UpgradeScript : MonoBehaviour
             else
             {
                 //スコアを消費
-                ScoreManager.score -= DiggingScore;
+                ScoreManager.score -= PlayerDataManager.DiggingScore;
                 //必要スコアを上昇
-                DiggingScore += 100;
+                PlayerDataManager.DiggingScore += 100;
                 //プレイヤーの採掘速度を上昇
                 E_PlayerController.digSpeed += 0.5f;
                 //レベル上昇
-                DiggingLevel += 1;
+                PlayerDataManager.DiggingLevel += 1;
 
                 ShowNotify($"採掘速度アップグレード完了");
             }
@@ -117,7 +87,7 @@ public class E_UpgradeScript : MonoBehaviour
         {
             //--移動速度強化
             //スコアが足りない場合
-            if(ScoreManager.score < SpeedScore)
+            if(ScoreManager.score < PlayerDataManager.SpeedScore)
             {
                 ShowNotify($"スコアが足りません");
             }
@@ -125,13 +95,13 @@ public class E_UpgradeScript : MonoBehaviour
             else
             {
                 //スコアを消費
-                ScoreManager.score -= SpeedScore;
+                ScoreManager.score -= PlayerDataManager.SpeedScore;
                 //必要スコアを上昇
-                SpeedScore += 100;
+                PlayerDataManager.SpeedScore += 100;
                 //プレイヤーの移動速度を上昇
                 E_PlayerController.MoveSpeed += 0.5f;
                 //レベル上昇
-                SpeedLevel += 1;
+                PlayerDataManager.SpeedLevel += 1;
 
                 ShowNotify($"移動速度アップグレード完了");
             }
@@ -141,7 +111,7 @@ public class E_UpgradeScript : MonoBehaviour
         {
             //--採掘範囲強化
             //スコアが足りない場合
-            if (ScoreManager.score < RangeScore)
+            if (ScoreManager.score < PlayerDataManager.RangeScore)
             {
                 ShowNotify($"スコアが足りません");
             }
@@ -149,13 +119,13 @@ public class E_UpgradeScript : MonoBehaviour
             else
             {
                 //スコアを消費
-                ScoreManager.score -= RangeScore;
+                ScoreManager.score -= PlayerDataManager.RangeScore;
                 //必要スコアを上昇
-                RangeScore += 100;
+                PlayerDataManager.RangeScore += 100;
                 //プレイヤーの移動速度を上昇
                 E_PlayerController.digRange += 0.5f;
                 //レベル上昇
-                RangeLevel += 1;
+                PlayerDataManager.RangeLevel += 1;
 
                 ShowNotify($"採掘範囲アップグレード完了");
             }
@@ -164,8 +134,8 @@ public class E_UpgradeScript : MonoBehaviour
         //Enterで次の画面へ移動
         if (Keyboard.current.enterKey.wasPressedThisFrame)
         {
-            //移動時自身をfalseにする
-            this.gameObject.SetActive(false);
+            ////移動時自身をfalseにする
+            //this.gameObject.SetActive(false);
             //シーン移動
             SceneManager.LoadScene(nextSceneName);
         }
---
+++ Cave_Miner/Assets/edakura/E_Script/PlayerDataManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerDataManager : MonoBehaviour
+{
+    //アップグレードに必要なスコア変数(強化毎に上昇)
+    //初期値は100でおいておく
+    public static int DiggingScore = 100;
+    public static int SpeedScore   = 100;
+    public static int RangeScore   = 100;
+    //各種強化レベル
+    //初期値はLv1でおいておく
+    public static int DiggingLevel = 1;
+    public static int SpeedLevel   = 1;
+    public static int RangeLevel   = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
