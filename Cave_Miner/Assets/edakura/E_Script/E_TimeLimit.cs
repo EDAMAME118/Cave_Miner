aa549d7557264352ae34dc66735c47cb1f36f58c
EDAMAME118 <h1202519011@hiratagakuen.onmicrosoft.com>
2026-05-13 12:06:43 +0900

    制限時間を0:00のフォーマットに改善した　アップグレードシーンキー入力表示追加

--- Cave_Miner/Assets/edakura/E_Script/E_TimeLimit.cs
+++ Cave_Miner/Assets/edakura/E_Script/E_TimeLimit.cs
@@ -4,13 +4,17 @@ using UnityEngine.UI;
 
 public class E_TimeLimit : MonoBehaviour
 {
-    //public MonoBehaviour upgradeScript;
-
+    //時間制限
     public int TimeLimit;
+    private int secondsTime;
+    private int minutesTime;
+
+    //1秒計測
     private float timeInterval;
 
     public Text timeText;
 
+    //制限時間が０になったらアップグレードシーンへ移動する用
     public string nextSceneBuy;
 
 
@@ -23,22 +27,26 @@ public class E_TimeLimit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeText.text = $"残り時間:{TimeLimit}秒";
+        //分と秒に変換
+        minutesTime = TimeLimit / 60;
+        secondsTime = TimeLimit % 60;
+
+        //現在の制限時間表示
+        timeText.text = $"残り時間 {minutesTime}:{secondsTime:D2}";
 
         timeInterval += Time.deltaTime;
 
+        //1秒経過時
         if(timeInterval >= 1.0f)
         {
+            //制限時間１減らす
             TimeLimit--;
+            //１秒計測変数をもとに戻す
             timeInterval -= 1.0f;
         }
 
         if(TimeLimit < 0)
         {
-            //upgradeScript.enabled = !upgradeScript.enabled;
-
-            Debug.Log("UpgradeScriptの状態を逆");
-
             SceneManager.LoadScene(nextSceneBuy);
         }
 
