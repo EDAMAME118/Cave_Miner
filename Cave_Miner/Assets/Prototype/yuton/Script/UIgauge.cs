a4a4c6ec02896267dcf4447b5eef418337d1688d
yuton794 <yutonosumahoumaho@icloud.com>
2026-06-02 12:22:36 +0900

    a

--- Cave_Miner/Assets/Prototype/yuton/Script/UIgauge.cs
+++ Cave_Miner/Assets/Prototype/yuton/Script/UIgauge.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class UIgage : MonoBehaviour
+public class UIgauge : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
