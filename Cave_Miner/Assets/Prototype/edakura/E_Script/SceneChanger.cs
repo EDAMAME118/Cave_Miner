e1868185e2b96b64f6f54fc45325d66e2f1b9afd
枝倉:ゲーム開発2 <h1202519011@hiratagakuen.onmicrosoft.com>
2026-07-02 15:55:26 +0900

    操作説明シーン追加

---
+++ Cave_Miner/Assets/Prototype/edakura/E_Script/SceneChanger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    //移動先のシーンをUnityから設定するw
+    [SerializeField]
+    private string sceneName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Update is called once per frame
+    void Update()
+    {
+        //エンターキーで設定したシーンに移動
+        if(Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
