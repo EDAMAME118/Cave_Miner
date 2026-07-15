c7987bc794880353aaf9b91e38c71ece27bb59d0
DESKTOP-8HAQ14J\GA2B <yutonosumahoumaho@icloud.com>
2026-07-15 16:16:41 +0900

    アニメーション頑張ってます

--- Cave_Miner/Assets/Prototype/edakura/E_Assets/E_Player/E_PlayerController.cs
+++ Cave_Miner/Assets/Prototype/edakura/E_Assets/E_Player/E_PlayerController.cs
@@ -9,6 +9,9 @@ public class E_PlayerController : MonoBehaviour
 
     public InputAction MoveAction;
     public InputAction DigAction;
+    Animator animator;
+    bool isMoveing = false;
+    int direction = 0;
 
     Vector2 PlayerVector;
     Rigidbody2D rbody;
@@ -30,10 +33,11 @@ public class E_PlayerController : MonoBehaviour
     {
         rbody = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
+        animator= GetComponent<Animator>();
 
         MoveAction.Enable();
         DigAction.Enable();
-
+        
         currentSpeed = 0.0f;
     }
 
@@ -44,32 +48,46 @@ public class E_PlayerController : MonoBehaviour
         PlayerVector = MoveAction.ReadValue<Vector2>();
 
         currentSpeed = PlayerDataManager.Instance.playerSpeed;
-
+        int dir=0;
         //向きの調整
         if (PlayerVector.x > 0.0f)
         {
             //Debug.Log("右移動");
             transform.rotation = Quaternion.Euler(0, 0, 90);
+            //右
+            dir = 3;
         }
         else if (PlayerVector.x < 0.0f)
         {
             //Debug.Log("左移動");
             transform.rotation = Quaternion.Euler(0, 0, -90);
+            //左
+            dir = 1;
         }
         else if (PlayerVector.y > 0.0f)
         {
             //Debug.Log("上移動");
             transform.rotation = Quaternion.Euler(0, 0, -180);
+            //上
+            dir = 2;
         }
         else if (PlayerVector.y < 0.0f)
         {
             //Debug.Log("下移動");
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            //下
+            dir = 0;
+        }
+        if (dir != direction)
+        {
+            direction = dir;
+            animator.SetInteger("Direction", direction);
         }
 
         //プレイヤーが移動している場合
         if (PlayerVector != Vector2.zero)
         {
+            isMoveing = true;
             //足音用変数加算
             walkAudioDelay += Time.deltaTime;
             if (walkAudioDelay > 0.5f)
@@ -82,7 +100,10 @@ public class E_PlayerController : MonoBehaviour
         }
         //立ち止まっている場合
         else
+        {
             walkAudioDelay = 0.0f;
+            isMoveing = false;
+        }
     }
 
     private void FixedUpdate()
